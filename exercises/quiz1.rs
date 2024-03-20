1eb16efe33cd0b0c3498268ae87562f6d53250e1
XenophonLXH <lourenzo321@gmail.com>
2024-03-20 07:46:20 +0200

    [TRAIN] quiz1: Small quiz using return types, if statements, comparisons and arithmetic operations

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,17 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
+fn calculate_price_of_apples(apple_count: i32) -> i32 {
+    let mut multi: i32 = 2;
+
+    if apple_count > 40 {
+        multi = 1
+    }
+
+    apple_count * multi
+}
 
 // Don't modify this function!
 #[test]
